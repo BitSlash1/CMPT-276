9eeb7857efbb7f166e059439d201d94223742e85
Alex Zhao <alex_zhao@sfu.ca>
2019-10-15 20:27:12 -0700

    Tokimon initial code

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -10,6 +27,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/tokimon', (req,res) => {
+    var getUsersQuery = `SELECT * FROM tokimon`;
+    console.log(getUsersQuery);
+    pool.query(getUsersQuery, (error, result) => {
+    if (error)
+      res.end(error);
+    var results = {'rows': result.rows };
+    console.log(results);
+    res.render('pages/tokimon', results)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
